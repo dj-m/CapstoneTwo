c7517baa0233462a79173168dafbc7cd6258ea93
Domingo José Moronta <domingomoronta@gmail.com>
2020-07-17 10:21:46 -0700

    Created cookiecutter structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Predictors of test performance for specialized high school admissions.',
+    author='Domingo Moronta',
+    license='MIT',
+)
